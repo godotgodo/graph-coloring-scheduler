ae6ba6a796563398f67cb8aa1cc18d565fa29ff2
serhatakcadag <serhat.akcadag@gmail.com>
2023-12-23 13:00:37 +0300

    Duplicate classes control before post requests

--- src/app/services/GivenSubjectService.js
+++ src/app/services/GivenSubjectService.js
@@ -1,6 +1,7 @@
 const { Service } = require("./service");
 const Subject = require("../models/Subject");
 const connectDB = require("@/config/db");
+const isSubjectUsed = require("@/utils/isSubjectUsed");
 
 class GivenSubjectService extends Service {
   constructor(model) {
@@ -10,10 +11,23 @@ class GivenSubjectService extends Service {
   async insert(data) {
     try {
       await connectDB();
-      const { lecturer } = await Subject.findById(data.subject);
-      data.lecturer = lecturer;
-      const res = await this.model.create(data);
-      return Response.json(res, { status: 200 });
+      const isUsed = await Subject.findById(data.subject);
+      if(isUsed)
+      {
+        return Response.json({message: "This subject is already in use."}, { status: 400 });
+      }
+      else
+      {
+        const { lecturer } = await Subject.findById(data.subject);
+        data.lecturer = lecturer;
+        const givenSubjectsByTheLecturer = await this.model.find({lecturer: data.lecturer});
+        if(isSubjectUsed(data, givenSubjectsByTheLecturer))
+        {
+          return Response.json({message: "There is already a given class at this time."}, { status: 400 });
+        }
+        const res = await this.model.create(data);
+        return Response.json(res, { status: 200 });
+      }
     } catch (e) {
       return Response.json(e, { status: 400 });
     }
