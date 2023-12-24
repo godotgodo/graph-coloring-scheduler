3440f9ef47096ebd64cf1d306633084d75da4832
godotgodo <yunusemregolbasi0@gmail.com>
2023-12-24 19:15:18 +0300

    Scheduler table development

---
+++ src/app/scheduler/page.js
@@ -0,0 +1,14 @@
+import SchedulerContainer from '@/components/scheduler/SchedulerContainer'
+import React from 'react'
+
+
+function Page() {    
+    
+  return (
+    <div className='w-full h-full min-h-screen'>
+        <SchedulerContainer/>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/components/scheduler/SchedulerContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SchedulerTable from "./SchedulerTable";
+
+function SchedulerContainer() {
+  return (
+    <div className="container mx-auto mt-3">
+      <SchedulerTable />
+    </div>
+  );
+}
+
+export default SchedulerContainer;
