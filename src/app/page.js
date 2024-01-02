dd73ee929a4cc19a754e57bf3d6422e849d92317
godotgodo <yunusemregolbasi0@gmail.com>
2024-01-02 23:53:06 +0300

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import Link from "next/link";
 
 export default function Home() {
-  if(!process.env.NEXT_BASE_API_URL) return null;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
